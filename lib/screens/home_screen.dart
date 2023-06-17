5ea2cdb7a5b4fa44d66a0822ed76d55ee6ab0b4f
An Nguyen <anphuong1501@gmail.com>
2023-06-17 20:51:55 +0700

    home screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,6 +21,7 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Center(
               child: Container(
@@ -49,7 +50,7 @@ class HomeScreen extends StatelessWidget {
               "28\u00B0",
               style: Theme.of(context).textTheme.displayLarge,
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
             SizedBox(
               height: 90,
               child: Column(
@@ -81,7 +82,86 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 10),
-            CustomWeatherBox()
+            CustomWeatherBox(),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Weekly forecast",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_right_alt_rounded,
+                    size: 30,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      WeeklyBox(),
+                      const SizedBox(width: 20),
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WeeklyBox extends StatelessWidget {
+  const WeeklyBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.5, vertical: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "25\u00B0",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            Image.network(
+              "https://img.icons8.com/?size=512&id=83424&format=png",
+              width: 20,
+              height: 20,
+            ),
+            const SizedBox(height: 7),
+            Text(
+              "17 June",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
